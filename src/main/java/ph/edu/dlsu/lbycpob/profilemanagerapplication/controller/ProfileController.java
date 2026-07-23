fcfd4df54bc0e8a95d20f7b39a49389bbd695d79
rheyan-prim <rheyan_prim@dlsu.edu.ph>
2026-07-23 22:45:46 +0800

    Update ProfileController.java
    corrected the import section to properly call profilemanagerapplication

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/ProfileController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/ProfileController.java
@@ -3,10 +3,10 @@ package ph.edu.dlsu.lbycpob.profilemanagerapplication.controller;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import ph.edu.dlsu.lbycpob.profilemanager.dto.Dtos;
-import ph.edu.dlsu.lbycpob.profilemanager.dto.Dtos.*;
-import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
-import ph.edu.dlsu.lbycpob.profilemanager.service.ProfileService;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.dto.Dtos;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.dto.Dtos.*;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Profile;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.service.ProfileService;
 
 import java.util.List;
 import java.util.Map;
