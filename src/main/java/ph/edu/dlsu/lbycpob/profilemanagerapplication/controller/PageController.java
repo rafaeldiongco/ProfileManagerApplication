515fdacb89de922d9f67a05e8913279368b7e096
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-07-23 15:11:41 +0800

    Pasted PageController from module

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/PageController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/PageController.java
@@ -1,4 +1,13 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
