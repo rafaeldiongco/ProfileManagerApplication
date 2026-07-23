63df4378a60a0d8386bbe678c6e512862a83e5ee
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-07-23 14:46:46 +0800

    This is the initial creation of the project

---
+++ src/test/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/ProfileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.profilemanagerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
